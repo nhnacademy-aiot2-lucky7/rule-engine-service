8ac2621c8d5105413c08fa91999c15b2df4f2d72
jy37414 <pjy37414@gmail.com>
2025-05-15 16:33:52 +0900

    잘못 삭제된 파일 복구.

---
+++ src/test/java/com/nhnacademy/ruleengineservice/RuleEngineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.ruleengineservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuleEngineServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
